a0ee6e063cd47c1f4c8e4cef32b376b9f0b2e750
redmachine2 <jlynn911@gmail.com>
2022-09-20 08:13:59 -0500

    add footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-main>
       <HelloWorld/>
     </v-main>
+    <footer class="footer">Lolchest isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</footer>
   </v-app>
 </template>
 
@@ -21,3 +22,11 @@ export default {
   }),
 }
 </script>
+
+<style scoped>
+.footer {
+  font-style: italic;
+  font-size: 12px;
+  padding: 0 25%;
+}
+</style>
